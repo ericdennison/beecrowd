7aefda3a53ebfd3733c1c3a20e0f0be035d2a0c3
eddesks <ericdenniso@gmail.com>
2024-05-31 08:55:42 -0300

    criado p14consumo

---
+++ p14consumo.py
@@ -0,0 +1,7 @@
+
+# Calcule o consumo médio de um automóvel sendo fornecidos a distância total percorrida (em Km) e o total de combustível gasto (em litros).
+
+x = int(input())
+y = float(input())
+kml = x/y
+print(f'{kml:.3f} km/l')

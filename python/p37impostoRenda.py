274ba4104a6906542c3425564174b55bcd84908e
eddesks <ericdenniso@gmail.com>
2024-09-20 16:43:38 -0300

    p37 incompleto sendo processado

---
+++ python/p37impostoRenda.py
@@ -0,0 +1,31 @@
+valor = 4520.00
+resultado = ((1000.00 / 100) * 8) + ((2.00 / 100)*18)
+resultado2 = ((1000.00 / 100) * 8) + ((1500 / 100) *18) + ((20.00 / 100)*28)
+print(resultado)
+print(resultado2)
+
+
+'''
+valor_string = str(valor)
+    print(valor_string[1:4]) 
+    valor_int = int(valor_string[1:4])
+    if valor_int > 500:
+        valor_int 
+        print('so testando')
+
+'''
+
+
+if valor > 4500:
+    ...
+
+elif valor >= 3000.01 and valor <= 4500.00:
+    ...
+elif valor >= 2000.01 and valor <= 3000.00:
+    ...
+elif valor >= 0.00 and valor <= 2000.00:
+    ...
+
+
+
+

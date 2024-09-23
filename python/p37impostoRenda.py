a6880a255a3201ea291d3a81079e558135c3f167
eddesks <ericdenniso@gmail.com>
2024-09-23 16:18:55 -0300

    p37 incompleto continua em analise

--- python/p37impostoRenda.py
+++ python/p37impostoRenda.py
@@ -1,21 +1,20 @@
 valor = 4520.00
-resultado = ((1000.00 / 100) * 8) + ((2.00 / 100)*18)
-resultado2 = ((1000.00 / 100) * 8) + ((1500 / 100) *18) + ((20.00 / 100)*28)
-print(resultado)
-print(resultado2)
+# resultado = ((1000.00 / 100) * 8) + ((2.00 / 100)*18)
+# resultado2 = ((1000.00 / 100) * 8) + ((1500 / 100) *18) + ((20.00 / 100)*28)
+# print(resultado)
+# print(resultado2)
 
 
-'''
-valor_string = str(valor)
-    print(valor_string[1:4]) 
-    valor_int = int(valor_string[1:4])
-    if valor_int > 500:
-        valor_int 
-        print('so testando')
+# valor_string = str(valor)
+# print(valor_string[1:4]) 
+# valor_int = int(valor_string[1:4])
+# valor_int2 = int(valor_string[0:4])
+# if valor_int > 500:
+#     valor_int 
+#     print('so testando')
         
 
-'''
-
+lista = []
 
 if valor > 4500:
     ...
@@ -26,6 +25,8 @@ elif valor >= 2000.01 and valor <= 3000.00:
     ...
 elif valor >= 0.00 and valor <= 2000.00:
     ...
+
+
 
 
 

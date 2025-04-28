7183198f0eda811bf8dc2a980ffd4a7d136569d9
eddesks <ericdenniso@gmail.com>
2025-04-28 16:53:07 -0300

    continuar depois

---
+++ python/p41tempoEvento.py
@@ -0,0 +1,11 @@
+
+dia_inicio = 5
+dia_fim = 9
+
+print(f'Dia {dia_inicio}')
+hh,mm,ss = map(int,input().split(':'))
+
+print(f'Dia {dia_fim}')
+hh,mm,ss = map(int,input().split(':'))
+
+print(hh, mm, ss)
